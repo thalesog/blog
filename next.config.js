d963712cf9ed6c4e48bfb2c5e182f39486fb6f81
Thales Ogliari <thalesog@me.com>
2022-01-10 13:24:54 -0300

    fix: preact setup

--- next.config.js
+++ next.config.js
@@ -2,22 +2,23 @@
  * @type {import('next').NextConfig}
  */
 
-const { withContentlayer } = require('next-contentlayer');
+const { withContentlayer } = require("next-contentlayer");
 
 module.exports = withContentlayer()({
   reactStrictMode: true,
   webpack: (config, { dev, isServer }) => {
     if (!dev && !isServer) {
       Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
+        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+        react: "preact/compat",
+        "react-dom": "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
       });
     }
 
     return config;
   },
   images: {
-    domains: ['github.com'],
+    domains: ["github.com"],
   },
 });
